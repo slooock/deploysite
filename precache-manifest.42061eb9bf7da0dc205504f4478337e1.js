646c28e6f6b23b1e71e67963f2a9f370b59e635f
Kayque <kayque.aavelar@gmail.com>
2020-08-24 16:58:53 -0300

    Updates

---
+++ precache-manifest.42061eb9bf7da0dc205504f4478337e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0f4b8e6df46c097460f919100110dc",
+    "url": "/slooock.github.io/index.html"
+  },
+  {
+    "revision": "4e1bc390fcba541074dc",
+    "url": "/slooock.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f732cadc46ded82f6c4a",
+    "url": "/slooock.github.io/static/js/2.a98ca589.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slooock.github.io/static/js/2.a98ca589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1bc390fcba541074dc",
+    "url": "/slooock.github.io/static/js/main.ca191d04.chunk.js"
+  },
+  {
+    "revision": "bc199e3adf8890ae2c53",
+    "url": "/slooock.github.io/static/js/runtime-main.90d7ec0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slooock.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);

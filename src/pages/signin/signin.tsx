7c183387da746bb4ce3ae04ab036403fbb71906c
kayque <kayque.avelar@somosagility.com.br>
2021-05-10 11:30:08 -0300

    finish

--- src/pages/signin/signin.tsx
+++ src/pages/signin/signin.tsx
@@ -9,25 +9,28 @@ import { Container } from './styles';
 const SignIn: React.FC = () => (
   <>
     <Container>
-    <a href="https://www.letras.mus.br/switchfoot/1247525/">
-      <h1>Created for a place I've never known</h1>
-    </a>
-     <span>
-       <a href="https://www.linkedin.com/in/slooock/">
-        <AiFillLinkedin size={50}/>
+      <a href="https://www.letras.mus.br/switchfoot/1247525/">
+        <h1>
+          Created for a place I've never known. After all my questions I'm gonna
+          call it home
+        </h1>
       </a>
-      <a href="https://github.com/slooock">
-        <AiFillGithub size={50}/>
-      </a>
-      <a href="https://twitter.com/SloooCk">
-        <GrTwitter size={50}/>
-      </a>
-      <a href="https://www.instagram.com/slooock/">
-        <AiFillInstagram size={50}/>
-      </a>
-     </span>
+      <span>
+        <a href="https://www.linkedin.com/in/slooock/">
+          <AiFillLinkedin size={50} />
+        </a>
+        <a href="https://github.com/slooock">
+          <AiFillGithub size={50} />
+        </a>
+        <a href="https://twitter.com/SloooCk">
+          <GrTwitter size={50} />
+        </a>
+        <a href="https://www.instagram.com/slooock/">
+          <AiFillInstagram size={50} />
+        </a>
+      </span>
     </Container>
   </>
-)
+);
 
 export default SignIn;
